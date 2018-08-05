fa4a79f20c0a4ad328a1000da0caf204dd5e8801
David Terry <me@xwvvvvwx.com>
2018-08-05 18:04:43 +0200

    move state to s3 bucket

--- main.tf
+++ main.tf
@@ -11,35 +11,64 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-humans-tcr"
+    key            = "tf-state"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-humans-tcr"
+    encrypt        = true
+    profile        = "humans-tcr"
+  }
+}
+
 #-------------------------------------------------------------------------------
-# WEBSITE
+# S3 BUCKET
 #-------------------------------------------------------------------------------
 
 resource "aws_s3_bucket" "site" {
   bucket = "humans-tcr"
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
   }
 }
 
+# resource "aws_s3_bucket_policy" "public_read" {
+#   bucket = "${aws_s3_bucket.site.id}"
+
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "PublicReadGetObject",
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": ["s3:GetObject"],
+#       "Resource": ["${aws_s3_bucket.site.arn}"]
+#     }
+#   ]
+# }
+# POLICY
+# }
+
 #-------------------------------------------------------------------------------
-# DEPLOYMENT USER
+# TRAVIS USER
 #-------------------------------------------------------------------------------
 
-resource "aws_iam_user" "deployer" {
-  name = "humans-deployer"
+resource "aws_iam_user" "travis" {
+  name = "humans-travis"
 }
 
-resource "aws_iam_access_key" "deployer" {
-  user    = "${aws_iam_user.deployer.name}"
+resource "aws_iam_access_key" "travis" {
+  user    = "${aws_iam_user.travis.name}"
   pgp_key = "keybase:xwvvvvwx"
 }
 
-resource "aws_iam_user_policy" "deployer" {
-  name = "humans-tcr-deployer"
-  user = "${aws_iam_user.deployer.name}"
+resource "aws_iam_user_policy" "travis" {
+  name = "humans-tcr-travis"
+  user = "${aws_iam_user.travis.name}"
 
   policy = <<EOF
 {
@@ -62,8 +91,4 @@ resource "aws_iam_user_policy" "deployer" {
   ]
 }
 EOF
-}
-
-output "deployer_secret_access_key" {
-  value = "${aws_iam_access_key.deployer.encrypted_secret}"
 }
