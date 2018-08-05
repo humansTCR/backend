0da637cd2f8ab488970f927b7e60489eadb8fa59
David Terry <me@xwvvvvwx.com>
2018-08-05 18:17:06 +0200

    add public read permissions to bucket

--- main.tf
+++ main.tf
@@ -34,24 +34,24 @@ resource "aws_s3_bucket" "site" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "public_read" {
-#   bucket = "${aws_s3_bucket.site.id}"
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = "${aws_s3_bucket.site.id}"
 
-#   policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Sid": "PublicReadGetObject",
-#       "Effect": "Allow",
-#       "Principal": "*",
-#       "Action": ["s3:GetObject"],
-#       "Resource": ["${aws_s3_bucket.site.arn}"]
-#     }
-#   ]
-# }
-# POLICY
-# }
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["${aws_s3_bucket.site.arn}/*"]
+    }
+  ]
+}
+POLICY
+}
 
 #-------------------------------------------------------------------------------
 # TRAVIS USER
